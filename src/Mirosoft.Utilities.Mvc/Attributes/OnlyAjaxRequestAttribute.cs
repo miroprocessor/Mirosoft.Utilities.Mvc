1ee4f021e0ca4e9361843f444fa7ca07653aa386
Amir Ismail <eng.amir.ismail@gmai.com>
2018-10-22 22:51:40 +0300

    add RequiredIf attribute

--- src/Mirosoft.Utilities.Mvc/Attributes/OnlyAjaxRequestAttribute.cs
+++ src/Mirosoft.Utilities.Mvc/Attributes/OnlyAjaxRequestAttribute.cs
@@ -7,7 +7,7 @@ namespace Mirosoft.Utilities.Mvc.Attributes
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class OnlyAjaxRequestAttribute : ActionMethodSelectorAttribute
     {
-        public override Boolean IsValidForRequest(ControllerContext context, MethodInfo info)
+        public override bool IsValidForRequest(ControllerContext context, MethodInfo info)
         {
             return context.HttpContext.Request.IsAjaxRequest();
         }
---
+++ src/Mirosoft.Utilities.Mvc/Attributes/RequiredIfAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mirosoft.Utilities.Mvc.Attributes
+{
+    public class RequiredIfAttribute : ValidationAttribute
+    {
+        private string PropertyName { get; set; }
+        private object DesiredValue { get; set; }
+
+        public RequiredIfAttribute(string propertyName, object desiredvalue, string errormessage)
+        {
+            PropertyName = propertyName;
+            DesiredValue = desiredvalue;
+            ErrorMessage = errormessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            var instance = context.ObjectInstance;
+            var type = instance.GetType();
+            var proprtyvalue = type.GetProperty(PropertyName).GetValue(instance, null);
+            if (proprtyvalue.ToString() == DesiredValue.ToString() && value == null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
